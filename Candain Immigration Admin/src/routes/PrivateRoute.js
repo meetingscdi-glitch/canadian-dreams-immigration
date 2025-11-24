9e81958c81ceb35649df59d8fa3d416679db01a7
mukesh_bhandari <mukeshbhandari@rowthautos.com>
2025-11-24 21:59:55 +0530

    v

--- Candain Immigration Admin/src/routes/PrivateRoute.js
+++ Candain Immigration Admin/src/routes/PrivateRoute.js
@@ -1,34 +1,61 @@
 import { Navigate, useLocation } from 'react-router-dom';
 import { APICore } from '../helpers/api/apiCore';
 
-type PrivateRouteProps = {
-    component: React.ComponentType,
-    roles?: string,
-};
-
-/**
- * Private Route forces the authorization before the route can be accessed
- * @param {*} param0
- * @returns
- */
-const PrivateRoute = ({ component: RouteComponent, roles, ...rest }: PrivateRouteProps) => {
+const PrivateRoute = ({ component: RouteComponent, roles, ...rest }) => {
     let location = useLocation();
     const api = new APICore();
     const loggedInUser = api.getLoggedInUser();
-    /**
-     * not logged in so redirect to login page with the return url
-     */
-    if (api.isUserAuthenticated() === false) {
-        return <Navigate to={'/account/login'} state={{ from: location }} replace />;
+
+    // not logged in → redirect to login
+    if (!api.isUserAuthenticated()) {
+        return <Navigate to="/account/login" state={{ from: location }} replace />;
     }
 
-    // check if route is restricted by role
-    if (roles && roles.indexOf(loggedInUser.role) === 'Admin') {
-        // role not authorised so redirect to home page
-        return <Navigate to={{ pathname: '/' }} />;
+    // check role permissions
+    if (roles && !roles.includes(loggedInUser.role)) {
+        return <Navigate to="/" replace />;
     }
 
     return <RouteComponent />;
 };
 
 export default PrivateRoute;
+    
+
+// import { Navigate, useLocation } from 'react-router-dom';
+// import { APICore } from '../helpers/api/apiCore';
+
+// type PrivateRouteProps = {
+//     component: React.ComponentType,
+//     roles?: string,
+// };
+
+// /**
+//  * Private Route forces the authorization before the route can be accessed
+//  * @param {*} param0
+//  * @returns
+//  */
+// const PrivateRoute = ({ component: RouteComponent, roles, ...rest }: PrivateRouteProps) => {
+//     let location = useLocation();
+//     const api = new APICore();
+//     const loggedInUser = api.getLoggedInUser();
+//     /**
+//      * not logged in so redirect to login page with the return url
+//      */
+//     if (api.isUserAuthenticated() === false) {
+//         return <Navigate to={'/account/login'} state={{ from: location }} replace />;
+//     }
+//     if (roles && !roles.includes(loggedInUser.role)) {
+//         return <Navigate to="/" replace />;
+//     }
+    
+//     // c    heck if route is restricted by role
+//     // if (roles && roles.indexOf(loggedInUser.role) === 'Admin') {
+//     //     // role not authorised so redirect to home page
+//     //     return <Navigate to={{ pathname: '/' }} />;
+//     // }
+
+//     return <RouteComponent />;
+// };
+
+// export default PrivateRoute;
